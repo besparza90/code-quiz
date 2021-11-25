40c7f9531cdba4890605f3a5082cf06b04ef4c81
besparza <esparzabrian90@gmail.com>
2021-11-24 16:11:42 -0800

    added HTML and questions

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+
+
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts","numbers"],
+        answer: 'alerts'
+    },
+{
+    question: "The condition of an if/else statement is enclosed with ___",
+    choices: ["quotes", "curly brackets","parenthesis", "square brackets"],
+    answer: "parenthesis"
+},
+{
+    question: "Arrays in JavaScript can be used to store___",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+},
+{
+    question: "String values must be enclosed within ___ when being assigned to variables",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"
+},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log"
+},
+]
